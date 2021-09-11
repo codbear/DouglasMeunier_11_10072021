792036150102b73db937f8644572ff85d3c77639
codbear <douglas.meunier@openclassrooms.com>
2021-09-12 01:14:35 +0200

    feat(errors): create NotFound screen

--- src/Modules/errors/screens/NotFoundScreen.jsx
+++ src/Modules/errors/screens/NotFoundScreen.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import { ROUTES } from '../../router';
 import { Layout } from '../../layout';
+import './NotFoundScreen.css';
 
 class NotFoundScreen extends React.Component {
   render() {
     return (
       <Layout>
-        <h1>Erreur 404</h1>
+        <div className="NotFoundScreen_root">
+          <p className="NotFoundScreen_errorCode">404</p>
+          <p className="NotFoundScreen_errorMessage">
+            Oups! La page que vous demandez n'existe pas.
+          </p>
+          <Link to={ROUTES.INDEX}>Retourner sur la page d'accueil</Link>
+        </div>
       </Layout>
     );
   }
