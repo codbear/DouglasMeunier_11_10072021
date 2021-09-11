12b0710052a20138d630f6c1e2ca2af0fb5bed59
codbear <douglas.meunier@openclassrooms.com>
2021-09-11 19:24:36 +0200

    feat(router): create router and empty screens

---
+++ src/Modules/errors/screens/NotFoundScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+const NotFoundScreen = ({}) => {
+  return <h1>Erreur 404</h1>;
+};
+
+NotFoundScreen.propTypes = propTypes;
+NotFoundScreen.defaultProps = defaultProps;
+
+export default NotFoundScreen;
---
+++ src/Modules/lodgings/screens/LodgingScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+const LodgingScreen = ({}) => {
+  return <h1>Logement</h1>;
+};
+
+LodgingScreen.propTypes = propTypes;
+LodgingScreen.defaultProps = defaultProps;
+
+export default LodgingScreen;
