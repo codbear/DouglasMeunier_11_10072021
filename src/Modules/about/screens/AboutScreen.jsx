43dcff3facf004430b5301207b852f0eaf01980c
codbear <douglas.meunier@openclassrooms.com>
2021-09-13 01:01:52 +0200

    feat(about): create About screen

--- src/Modules/about/screens/AboutScreen.jsx
+++ src/Modules/about/screens/AboutScreen.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 
 import { Layout } from '../../layout';
+import { Accordion } from '../../accordion';
+import HeroBackground from '../images/kalen-emsley-Bkci_8qcdvQ-unsplash 2.png';
+import './AboutScreen.scss';
+
+const accordions = [
+  {
+    summary: 'Fiabilité',
+    details:
+      'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.',
+  },
+  {
+    summary: 'Respect',
+    details:
+      'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+  },
+  {
+    summary: 'Service',
+    details:
+      "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+  },
+  {
+    summary: 'Sécurité',
+    details:
+      "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+  },
+];
 
 class AboutScreen extends React.Component {
   render() {
     return (
       <Layout>
-        <h1>A propos</h1>
+        <img src={HeroBackground} alt="" className="About_hero" />
+        <div className="About_accordionsWrapper">
+          {accordions.map((accordion) => (
+            <Accordion
+              key={accordion.summary}
+              summary={accordion.summary}
+              details={accordion.details}
+            />
+          ))}
+        </div>
       </Layout>
     );
   }
