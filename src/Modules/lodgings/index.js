9544f4c0e4862ddc1380f4af3158cfca7f7bab3a
codbear <douglas.meunier@openclassrooms.com>
2021-09-13 19:58:49 +0200

    feat(lodgings): create withLodgingsData hoc

--- src/Modules/lodgings/index.js
+++ src/Modules/lodgings/index.js
@@ -1,1 +1,3 @@
 export { default as Lodging } from './screens/LodgingScreen';
+export { default as LodgingCard } from './components/LodgingCard/LodgingCard';
+export { default as withLodgingsData } from './hoc/withLodgingsData';
