9c5739854aae4ce272bb8000bbbcd7702c6c7f56
codbear <douglas.meunier@openclassrooms.com>
2021-09-13 20:00:35 +0200

    feat(home): create Home screen

--- src/Modules/home/screens/HomeScreen.jsx
+++ src/Modules/home/screens/HomeScreen.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Layout } from '../../layout';
+import { Hero } from '../../hero';
+import { LodgingCard, withLodgingsData } from '../../lodgings';
+import HeroBackground from '../images/hero-background.png';
+import './HomeScreen.scss';
 
 class HomeScreen extends React.Component {
+  static propTypes = {
+    lodgingsData: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string,
+        title: PropTypes.string,
+        cover: PropTypes.string,
+      })
+    ),
+  };
   render() {
+    const { lodgingsData } = this.props;
+
     return (
       <Layout>
-        <h1>Accueil</h1>
+        <div className="Home">
+          <Hero background={HeroBackground} textOverlay={['Chez vous,', 'partout et ailleurs']} />
+          <div className="Home_gallery">
+            {lodgingsData
+              ? lodgingsData.map((lodgingData) => (
+                  <LodgingCard
+                    key={lodgingData.id}
+                    title={lodgingData.title}
+                    cover={lodgingData.cover}
+                    lodgingId={lodgingData.id}
+                  />
+                ))
+              : 'Aucun logement trouvé'}
+          </div>
+        </div>
       </Layout>
     );
   }
 }
 
-export default HomeScreen;
+export default withLodgingsData(HomeScreen);
