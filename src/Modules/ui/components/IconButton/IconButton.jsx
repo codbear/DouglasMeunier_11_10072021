b03225d988adc733ba0ea8eaf040a6f1a16eb9a8
codbear <douglas.meunier@openclassrooms.com>
2021-09-15 10:45:02 +0200

    refactor(ui): create IconButton component

---
+++ src/Modules/ui/components/IconButton/IconButton.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './IconButton.scss';
+
+class IconButton extends React.Component {
+  static propTypes = {
+    onClick: PropTypes.func,
+    icon: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired,
+    iconProps: PropTypes.objectOf(PropTypes.any),
+    className: PropTypes.string,
+  };
+
+  static defaultProps = {
+    onClick: () => {},
+    iconProps: {},
+    className: '',
+  };
+
+  render() {
+    const { className, onClick, icon, iconProps, ...otherProps } = this.props;
+
+    return (
+      <button onClick={onClick} className={`IconButton ${className}`} type="button" {...otherProps}>
+        <img src={icon} alt="" aria-hidden {...iconProps} />
+      </button>
+    );
+  }
+}
+
+export default IconButton;
